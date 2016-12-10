de73948465dda0075d72d8e57197defd3068fa6b
Brennan <4brothersnosisters@gmail.com>
2016-12-09 22:07:44 -0500

    finished untested version of 12b

---
+++ Lab12/lab12b.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=lab12b.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/lab12b.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=lab12b.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/lab12b.x.tar
